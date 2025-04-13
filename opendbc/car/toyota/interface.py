e853fe8dc2e084c135e61468a506fce68193e6e4
rav4kumar <meetkumardesai@gmail.com>
2025-04-13 13:21:26 -0700

    do not set them here
    Signed-off-by: rav4kumar <meetkumardesai@gmail.com>

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -179,12 +179,12 @@ class CarInterface(CarInterfaceBase):
     if candidate in UNSUPPORTED_DSU_CAR:
       ret.safetyParam |= ToyotaSafetyFlagsSP.UNSUPPORTED_DSU
 
-    if candidate in (TSS2_CAR - SECOC_CAR):
-      ret.flags |= ToyotaFlagsSP.SP_ENHANCED_BSM.value
+    #if candidate in (TSS2_CAR - SECOC_CAR):
+    #  ret.flags |= ToyotaFlagsSP.SP_ENHANCED_BSM.value
     if candidate == CAR.TOYOTA_PRIUS_TSS2:
       ret.flags |= ToyotaFlagsSP.SP_NEED_DEBUG_BSM.value
-    if candidate in (TSS2_CAR - RADAR_ACC_CAR - SECOC_CAR):
-      ret.flags |= ToyotaFlagsSP.SP_AUTO_BRAKE_HOLD.value
+    #if candidate in (TSS2_CAR - RADAR_ACC_CAR - SECOC_CAR):
+    #  ret.flags |= ToyotaFlagsSP.SP_AUTO_BRAKE_HOLD.value
 
 
     return ret
