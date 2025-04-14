b95383e2e65faef2fb5d89a66638416f3353aa93
rav4kumar <meetkumardesai@gmail.com>
2025-04-14 07:59:54 -0700

    TSS2 - Refine long tune parameters for improved control dynamics
    Signed-off-by: rav4kumar <meetkumardesai@gmail.com>

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -161,7 +161,7 @@ class CarInterface(CarInterfaceBase):
       if sp_tss2_long_tune:
         ret.vEgoStopping = 0.15
         ret.vEgoStarting = 0.22
-        ret.stoppingDecelRate = 0.01  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.02  # reach stopping target smoothly
       else:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
